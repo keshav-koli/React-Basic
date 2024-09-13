f53fd2acc99206ad9e8ad7bcaad194f1854154a3
keshav-koli <keshavkoli5588@gmail.com>
2024-09-13 11:35:37 +0530

    changes in food.js

---
+++ food.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading=(
+// header 
+// body
+// footer
+<h1>
+    feel good
+</h1>
+);
+
+const root=ReactDOM.createRoot(document.querySelector('#root'));
+root.render(heading);
