7c987084f83daa609ee350f704bb8cd0ccbfe075
keshav-koli <keshavkoli5588@gmail.com>
2025-01-05 23:46:39 +0530

    update ResturantMenu component to fetch menu data using restaurant ID from URL; add menu_food_api constant for API endpoint

--- src/components/ResturantMenu.js
+++ src/components/ResturantMenu.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import Shimer from "./Shimer";
+import { useParams } from "react-router-dom";
+import { menu_food_api } from "../utils/constant";
 
 const ResturantMenu = () => {
   let [menuData, setmenuData] = useState(null);
@@ -8,17 +9,18 @@ const ResturantMenu = () => {
     fetch_menu_data();
   }, []);
 
+  const { resid } = useParams();
   // console.log("rendered");
+  console.log(resid)
   const fetch_menu_data = async () => {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6582349&lng=77.1432671&restaurantId=655882&catalog_qa=undefined&submitAction=ENTER"
-    );
+    const data = await fetch(menu_food_api + resid);
     const json = await data.json();
     // console.log(json);
     setmenuData(json.data);
     // console.log(json.data);
     console.log(
-      json.data.cards[4].groupedCard.cardGroupMap.REGULAR.cards[4].card.card.categories[0].itemCards
+      json.data.cards[4].groupedCard.cardGroupMap.REGULAR.cards[4].card.card
+        .categories[0].itemCards
     );
 
     // console.log(json.data.cards[2].card.card.info);
@@ -36,18 +38,15 @@ const ResturantMenu = () => {
   const { name, cuisines, costForTwoMessage } =
     menuData.cards[2].card.card.info;
 
-
-    if(!itemCards){
-  menuData.map((resinfo) => {
-    resinfo.data.card[4].groupedCard.cardGroupMap.REGULAR.cards[4].card.card.categories[0].itemCards.map(
-      (item) => {
-        console.log(item.card.info.name);
-      }
-    );
-  });
-
-  
-}
+  if (!itemCards) {
+    menuData.map((resinfo) => {
+      resinfo.data.card[4].groupedCard.cardGroupMap.REGULAR.cards[4].card.card.categories[0].itemCards.map(
+        (item) => {
+          console.log(item.card.info.name);
+        }
+      );
+    });
+  }
   const menu_food_img_id =
     "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/";
   return (
@@ -56,9 +55,21 @@ const ResturantMenu = () => {
       <h3>{cuisines.join(", ")}</h3>
       <h3>{itemCards[0].card.info.category}</h3>
       <div className="menu-data">
-      {itemCards.map(
-      (item) => <><h4>{item.card.info.name}{" - Rs "}{item.card.info.finalPrice / 100 || item.card.info.price / 100}</h4><img className="menu-data-image" src={menu_food_img_id.concat(item?.card?.info?.imageId)}></img></>
-      )}
+        {itemCards.map((item) => (
+          <div key={item.card.info.id}>
+            <h4>
+              {item.card.info.name}
+              {" - Rs "}
+              {item.card.info.finalPrice / 100 ||
+                item.card.info.price / 100 ||
+                item.card.info.defaultPrice / 100}
+            </h4>
+            <img
+              className="menu-data-image"
+              src={menu_food_img_id.concat(item?.card?.info?.imageId)}
+            ></img>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,5 +1,7 @@
 export const navbar_logo="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/91631e116794391.60698dbc55f96.jpg";
 const food_imageCDN="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" ;
 export default food_imageCDN;
+
+export const menu_food_api="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6582349&lng=77.1432671&restaurantId=";
 // EXPORT CONST NAVBAR_LOGO IS NAMED EXPORT
 // EXPORT DEFAULT FOOD_IMAGECDN IS DEFAULT EXPORT
