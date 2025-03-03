c18e446d21e0f109e02d17df16cad10036bd211e
keshav-koli <keshavkoli5588@gmail.com>
2025-03-03 21:27:32 +0530

    add React preset to Babel configuration; update Grocery component with a form and inputs; enhance tests for Grocery component

--- src/__test__/sum.test.js
+++ src/__test__/sum.test.js
@@ -1,7 +1,35 @@
+import { render, screen } from "@testing-library/react";
 import { sum } from "../components/sum";
+import Grocery from "../components/Grocery";
+import "@testing-library/jest-dom";
 
 test("Sum function should calculate the sum of two numbers", () => {
   const result = sum(3, 4);
-    //Assertion
+  //Assertion
   expect(result).toBe(7);
+});
+
+test("containing heading in the grocery  ", () => {
+  render(<Grocery />);
+
+  //Querying
+  const heading = screen.getByRole("heading");
+
+  expect(heading).toBeInTheDocument();
+});
+
+test("containing button in the grocery", () => {
+  render(<Grocery />);
+  const btn = screen.getByText("Submit");
+
+  //assertion
+  expect(btn).toBeInTheDocument();
+});
+
+test("contain 2 input tag", () => {
+  render(<Grocery />);
+  const input = screen.getAllByRole("textbox");
+  console.log(input.length)
+  // expect(input.length).toBe(2)
+  expect(input.length).not.toBe(3)
 });
--- src/components/Grocery.js
+++ src/components/Grocery.js
@@ -2,7 +2,13 @@ const Grocery = () => {
   return (
     <div className="grocery min-h-[520px]">
       <h1>Hello this is a grocery store</h1>
-      <h2>This is a grocery page</h2>
+      <form>
+        <input type="text" placeholder="Enter Name" className="border-2"></input><br/><br/>
+        <input type="text" placeholder="Enter password" className="border-2"></input><br/><br/>
+        <button type="submit" className="border-2">Submit</button>
+      </form>
+
+      {/* <h2>This is a grocery page</h2> */}
     </div>
   );
 };
